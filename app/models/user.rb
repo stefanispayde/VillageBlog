06b19dcf705277f0fffc1ef581f1b0115b84b20e
Stefani Spayde <stefani@bearandgiraffe.com>
2018-06-04 02:18:29 -0500

    created post and category models/created associations between models/created tables for new models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :posts, dependent: :destroy
+  
+
+
 # p User.new
 
 end
