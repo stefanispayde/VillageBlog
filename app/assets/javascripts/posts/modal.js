c1111625094cbfb81f0d00e84999bc28151424e8
Stefani Spayde <stefani@bearandgiraffe.com>
2018-06-07 05:38:14 -0500

    created a show.html.erb file for user to view entire single post, created a show action in PostController to redirect user to posts show file, created show.scss for styling

--- app/assets/javascripts/posts/modal.js
+++ app/assets/javascripts/posts/modal.js
@@ -4,11 +4,11 @@ $(document).on('turbolinks:load', function() {
     var posted_by = $(this).find('.post-content .posted-by').html();
     var post_heading = $(this).find('.post-content h3').html();
     var post_content = $(this).find('.post-content p').html();
-    var interested = $(this).find('.post-content .interested').attr('href');
+    var more = $(this).find('.post-content .more').attr('href');
     $('.modal-header .posted-by').text(posted_by);
     $('.loaded-data h3').text(post_heading);
     $('.loaded-data p').text(post_content);
-    $('.loaded-data .interested a').attr('href', interested);
+    $('.loaded-data .more a').attr('href', more);
     $('.myModal').modal('show');
   });
 });
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,6 @@ class WelcomeController < ApplicationController
   def index
     @posts = Post.limit(15)
   end
+
+  
 end
